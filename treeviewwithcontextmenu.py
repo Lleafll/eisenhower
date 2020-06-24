9f1f4d6e86eee9f44b5619fefbd2f579dc7d442e
Lars Lorenz <lars.lorenz@iws.fraunhofer.de>
2020-06-24 13:51:28 +0200

    Show sort indicators and make them interactable

--- treeviewwithcontextmenu.py
+++ treeviewwithcontextmenu.py
@@ -63,6 +63,8 @@ class TreeViewWithContextMenu(QtWidgets.QTreeView):
             "background-color: rgb(114, 118, 138);"
             "border: none;"
             "}")
+        self.header().setSortIndicatorShown(True)
+        self.header().setSectionsClickable(True)
 
     def columns(self) -> Sequence[Column]:
         return self._displayed_columns
