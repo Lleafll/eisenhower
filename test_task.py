7f00823f8d191ba88a4172f0b4422f00969b7b9a
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-09-29 22:39:25 +0200

    Fix unit tests which handled sub tasks

--- test_task.py
+++ test_task.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 from unittest import TestCase
 
-from task import Task, SubTask, is_urgent, has_snoozed_date
+from task import Task, is_urgent, has_snoozed_date
 
 
 class TestTask(TestCase):
@@ -9,87 +9,22 @@ class TestTask(TestCase):
         task = Task()
         self.assertIsNone(task.snooze)
 
-    def test_snooze_one_sub_task(self):
-        task = Task(sub_tasks=(SubTask(),))
-        self.assertIsNone(task.snooze)
-
-    def test_snooze_one_snoozed_sub_task(self):
-        task = Task(sub_tasks=(SubTask(snooze=date.today() + timedelta(days=2)), ))
-        self.assertEqual(task.snooze, date.today() + timedelta(days=2))
-
-    def test_snooze_several_snoozed_sub_task(self):
-        task = Task(sub_tasks=(
-            SubTask(snooze=date(9, 10, 11)),
-            SubTask(snooze=date(3, 4, 5)),
-            SubTask(snooze=date(6, 7, 8)),
-            SubTask(snooze=None)))
-        self.assertEqual(task.snooze, None)
-
     def test_due_empty_task(self):
         task = Task()
         self.assertIsNone(task.due)
-
-    def test_due_several_due_tasks(self):
-        task = Task(sub_tasks=(
-            SubTask(due=date(9, 10, 11)),
-            SubTask(due=date(3, 4, 5)),
-            SubTask(due=date(6, 7, 8))))
-        self.assertEqual(task.due, date(3, 4, 5))
-
-    def test_due_one_undue_task(self):
-        task = Task(sub_tasks=(
-            SubTask(due=date(9, 10, 11)),
-            SubTask(due=date(3, 4, 5)),
-            SubTask(due=date(6, 7, 8)),
-            SubTask(due=None)))
-        self.assertEqual(task.due, date(3, 4, 5))
-
-    def test_is_urgent_sub_task(self):
-        sub_task = SubTask()
-        self.assertFalse(is_urgent(sub_task))
 
     def test_is_urgent_task(self):
         task = Task()
         self.assertFalse(is_urgent(task))
 
-    def test_is_urgent_task_with_due_sub_task(self):
-        task = Task(sub_tasks=(SubTask(due=date(5, 3, 2)),))
+    def test_is_urgent_due_task(self):
+        task = Task(due=date(5, 3, 2))
         self.assertTrue(is_urgent(task))
 
-    def test_is_urgent_task_with_undue_sub_task(self):
-        task = Task(sub_tasks=(SubTask(),))
+    def test_is_urgent_undue_task(self):
+        task = Task(due=None)
         self.assertFalse(is_urgent(task))
 
     def test_is_urgent_task_in_the_distant_future(self):
-        task = Task(sub_tasks=(SubTask(due=date(9999, 1, 1)),))
+        task = Task(due=date(9999, 1, 1))
         self.assertFalse(is_urgent(task))
-
-    def test_uses_due_date_of_unsnoozed_subtask(self):
-        task = Task(sub_tasks=(
-            SubTask(due=date(1, 2, 3), snooze=date.today() + timedelta(days=2)),
-            SubTask(due=date(4, 5, 6))))
-        self.assertEqual(task.due, date(4, 5, 6))
-
-    def test_uses_due_date_of_snoozed_subtasks_when_no_unsnoozed_subtask(self):
-        task = Task(sub_tasks=(
-            SubTask(due=date(1, 2, 3), snooze=date.today() + timedelta(days=2)),
-            SubTask(due=date(4, 5, 6), snooze=date.today() + timedelta(days=3))))
-        self.assertEqual(task.due, date(1, 2, 3))
-
-    def test_snooze_is_None_when_task_has_unsnoozed_subtask_without_due_date(self):
-        task = Task(sub_tasks=(
-            SubTask(due=date(1, 2, 3), snooze=date.today() + timedelta(days=2)),
-            SubTask(due=None, snooze=None)))
-        self.assertIsNone(task.snooze)
-
-    def test_is_not_snoozed_when_task_has_unsnoozed_subtask_without_due_date(self):
-        task = Task(sub_tasks=(
-            SubTask(due=date(1, 2, 3), snooze=date.today() + timedelta(days=2)),
-            SubTask(due=None, snooze=None)))
-        self.assertFalse(has_snoozed_date(task))
-
-    def test_is_not_snoozed_when_task_has_snoozed_subtask_in_the_past_without_due_date(self):
-        task = Task(sub_tasks=(
-            SubTask(due=date(1, 2, 3), snooze=date.today() + timedelta(days=2)),
-            SubTask(due=None, snooze=date(4, 5, 6))))
-        self.assertFalse(has_snoozed_date(task))
--- test_taskmanager.py
+++ test_taskmanager.py
@@ -2,27 +2,13 @@ from unittest import TestCase
 from PySide6 import QtCore
 from datetime import date
 
-from task import Task, SubTask
+from task import Task, SubTask, Importance
 from taskmanager import TaskManager, sanitize_sub_task
 
 
 class TestTaskManager(TestCase):
-    def test_add_sub_task(self):
-        task = Task()
-        task_manager = TaskManager([task])
-        task_manager.add_sub_task(task)
-        self.assertEqual(
-            task_manager.tasks(),
-            [Task(sub_tasks=(SubTask("New SubTask"),))])
-
-    def test_delete_sub_task(self):
-        sub_task = SubTask()
-        task_manager = TaskManager([Task(sub_tasks=(sub_task,))])
-        task_manager.delete_sub_task(sub_task)
-        self.assertEqual(
-            task_manager.tasks(),
-            [Task()])
-
     def test_sanitize_sub_task(self):
         sub_task = SubTask("Name", QtCore.QDate(2001, 12, 24))
-        self.assertEqual(sanitize_sub_task(sub_task), SubTask("Name", date(2001, 12, 24)))
+        self.assertEqual(
+            sanitize_sub_task(sub_task, Importance.Important, None),
+            Task("Name", importance=Importance.Important, due=date(2001, 12, 24)))
