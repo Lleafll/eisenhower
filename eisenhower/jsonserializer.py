0810079cf42497f7520abffad4082b355317b36b
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-12-23 00:41:48 +0100

    Add tests for JsonSerializer

--- eisenhower/jsonserializer.py
+++ eisenhower/jsonserializer.py
@@ -6,16 +6,17 @@ from task import Task, to_primitive_dicts, tasks_from_primitive_dicts
 
 
 class JsonSerializer:
-    def __init__(self, path: Path) -> None:
+    def __init__(self, path: Path, open_=open) -> None:
         self._path = path
+        self._open = open_
 
     def save(self, tasks: Sequence[Task]) -> None:
-        with open(self._path, "w") as file:
+        with self._open(self._path, "w") as file:
             json.dump(to_primitive_dicts(tasks), file, indent=4)
 
     def load(self) -> list[Task]:
         try:
-            with open(self._path, "r") as file:
+            with self._open(self._path, "r") as file:
                 return tasks_from_primitive_dicts(json.load(file))
         except FileNotFoundError:
             return []
---
+++ eisenhower/test_jsonserializer.py
@@ -0,0 +1,55 @@
+from io import StringIO
+from pathlib import Path
+from typing import IO, Type
+
+from jsonserializer import JsonSerializer
+from task import Task
+
+
+def build_mock_open(content: str = "") -> (StringIO, Type):
+    file_ = StringIO(content)
+
+    class MockOpen:
+        def __init__(self, _: Path, method: str) -> None:
+            assert method in ("w", "r")
+            self.file = file_
+
+        def __enter__(self) -> IO[str]:
+            return self.file
+
+        def __exit__(self, _, _2, _3) -> None:
+            pass
+
+    return file_, MockOpen
+
+
+def test_save() -> None:
+    file, mock_open = build_mock_open()
+    serializer = JsonSerializer(Path("path"), mock_open)
+    serializer.save([Task("kog")])
+    expected = """[
+    {
+        "name": "kog",
+        "importance": "Unimportant",
+        "completed": null,
+        "due": null,
+        "snooze": null
+    }
+]"""
+    assert file.getvalue() == expected
+
+
+def test_load() -> None:
+    content = """[
+    {
+        "name": "ztru",
+        "importance": "Unimportant",
+        "completed": null,
+        "due": null,
+        "snooze": null
+    }
+]"""
+    file, mock_open = build_mock_open(content)
+    serializer = JsonSerializer(Path("path"), mock_open)
+    tasks = serializer.load()
+    assert tasks == [Task("ztru")]
