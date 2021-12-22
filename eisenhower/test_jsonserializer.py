631d258c37d857ae41efe3013dd3003a3f5372fe
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-12-23 00:47:03 +0100

    Add JsonSerializer test for case when file does not exist

--- eisenhower/test_jsonserializer.py
+++ eisenhower/test_jsonserializer.py
@@ -53,3 +53,19 @@ def test_load() -> None:
     serializer = JsonSerializer(Path("path"), mock_open)
     tasks = serializer.load()
     assert tasks == [Task("ztru")]
+
+
+class FileNotFoundMockOpen:
+    def __init__(self, _: Path, _2: str) -> None:
+        raise FileNotFoundError
+
+    def __enter__(self) -> IO[str]:
+        return StringIO()
+
+    def __exit__(self, _, _2, _3) -> None:
+        pass
+
+
+def test_load_when_file_not_exists() -> None:
+    serializer = JsonSerializer(Path("path"), FileNotFoundMockOpen)
+    assert serializer.load() == []
