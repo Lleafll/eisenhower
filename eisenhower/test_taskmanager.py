d9a8af745810c906f4e45a4d1c4966b9ca0e2e86
Lars Lorenz <lars.ll.lorenz@gmail.com>
2021-12-22 13:07:34 +0100

    Add more TasksManager tests

--- eisenhower/test_taskmanager.py
+++ eisenhower/test_taskmanager.py
@@ -36,3 +36,67 @@ def test_set_complete() -> None:
     assert manager.tasks() == [Task("fj9", completed=date.today())]
     manager.set_complete(Task("fj9", completed=date.today()), False)
     assert manager.tasks() == [Task("fj9", completed=None)]
+
+
+def test_schedule_task() -> None:
+    manager = TaskManager([Task("g5")])
+    manager.schedule_task(Task("g5"), date(2, 5, 7))
+    assert manager.tasks() == [Task("g5", due=date(2, 5, 7))]
+    manager.schedule_task(Task("g5", due=date(2, 5, 7)), None)
+    assert manager.tasks() == [Task("g5", due=None)]
+
+
+def test_snooze() -> None:
+    manager = TaskManager([Task("fgjnio")])
+    manager.snooze(Task("fgjnio"), date(4, 8, 5))
+    assert manager.tasks() == [Task("fgjnio", snooze=date(4, 8, 5))]
+    manager.snooze(Task("fgjnio", snooze=date(4, 8, 5)), None)
+    assert manager.tasks() == [Task("fgjnio", snooze=None)]
+
+
+def test_rename() -> None:
+    manager = TaskManager([Task("gfio")])
+    manager.rename(Task("gfio"), "ggrgr")
+    assert manager.tasks() == [Task("ggrgr")]
+
+
+def test_remove_due() -> None:
+    manager = TaskManager([Task("bfg", due=date(5, 8, 1))])
+    manager.remove_due(Task("bfg", due=date(5, 8, 1)))
+    assert manager.tasks() == [Task("bfg")]
+
+
+def test_remove_snooze() -> None:
+    manager = TaskManager([Task("j54", snooze=date(5, 8, 1))])
+    manager.remove_snooze(Task("j54", snooze=date(5, 8, 1)))
+    assert manager.tasks() == [Task("j54")]
+
+
+def test_set_importance() -> None:
+    manager = TaskManager([Task("fio")])
+    manager.set_importance(Task("fio"), Importance.Important)
+    assert manager.tasks() == [Task("fio", importance=Importance.Important)]
+
+
+def test_is_undoable_after_add() -> None:
+    manager = TaskManager([])
+    assert not manager.is_undoable()
+    manager.add(Task())
+    assert manager.is_undoable()
+
+
+def test_is_redoable_after_undo() -> None:
+    manager = TaskManager([])
+    manager.add(Task())
+    assert not manager.is_redoable()
+    manager.undo()
+    assert manager.is_redoable()
+
+
+def test_redo() -> None:
+    manager = TaskManager([])
+    manager.add(Task("sop"))
+    manager.undo()
+    assert not manager.tasks()
+    manager.redo()
+    assert manager.tasks() == [Task("sop")]
